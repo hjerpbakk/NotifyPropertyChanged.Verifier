27eaa76bdac9eb7560be2187712f64635b63daeb
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2019-10-23 07:12:55 +0200

    Moved tests and example ViewModel to Tests folder, closes #1

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 rm -f -r ./CoverageResults
-dotnet test ./NotifyPropertyChanged.Verifier.Tests/NotifyPropertyChanged.Verifier.Tests.csproj --configuration Release /p:CollectCoverage=true /p:Exclude="[ViewModels]*" /p:CoverletOutput="../CoverageResults/"
-dotnet test ./NotifyPropertyChanged.Verifier.AdditionalTests/NotifyPropertyChanged.Verifier.AdditionalTests.csproj --configuration Release /p:CollectCoverage=true /p:Exclude="[ViewModels]*" /p:CoverletOutput="../CoverageResults/" /p:MergeWith="../CoverageResults/coverage.json" /p:CoverletOutputFormat=opencover /p:Threshold=94.44 
+dotnet test ./Tests/NotifyPropertyChanged.Verifier.Tests/NotifyPropertyChanged.Verifier.Tests.csproj --configuration Release /p:CollectCoverage=true /p:Exclude="[ViewModels]*" /p:CoverletOutput="../CoverageResults/"
+dotnet test ./Tests/NotifyPropertyChanged.Verifier.AdditionalTests/NotifyPropertyChanged.Verifier.AdditionalTests.csproj --configuration Release /p:CollectCoverage=true /p:Exclude="[ViewModels]*" /p:CoverletOutput="../CoverageResults/" /p:MergeWith="../CoverageResults/coverage.json" /p:CoverletOutputFormat=opencover /p:Threshold=94.44 
