b55611c8a14e8159a9ac33d8332e0e81233cea32
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2019-10-23 08:24:39 +0200

    Changed path of package, #4

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-dotnet pack ./NotifyPropertyChanged.Verifier/NotifyPropertyChanged.Verifier.csproj --configuration Release --no-restore --no-build
+dotnet pack ./NotifyPropertyChanged.Verifier/NotifyPropertyChanged.Verifier.csproj --configuration Release --no-restore --no-build --output nupkgs
